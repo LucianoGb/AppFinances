1ca5cda75326c913443777eb5160378d82949b0f
Luciano Gouveia <ljdgb2012@gmail.com>
2023-11-28 13:07:44 -0300

    Configura conexão ao DB

---
+++ app/config/constants.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+const DATABASE = [
+    'app_db' => 'u860510651_appFinances',
+    'app_user' => 'u860510651_adminAFdb',
+    'app_pw' => 'AdminAFBd@2023',
+    'app_ip' => '149.100.155.204',
+];
---
+++ app/models/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppFinances\models;
+
+class User
+{
+
+
+    protected string $name;
+    protected string $email;
+    protected string $password;
+
+
+}
